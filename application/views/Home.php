de25ac766f310df9a47399fbba44887f51500f8c
Abhishek Singh <47791443+thatdelhiboy@users.noreply.github.com>
2020-03-06 16:48:07 +0530

    Update Home.php

--- application/views/Home.php
+++ application/views/Home.php
@@ -1,83 +1,257 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed'); ?>
-<style>
-	body{
-		background-image:url(https://www.elsetge.cat/myimg/f/114-1145577_alphagamma-my-millennial-job-search-part-i-the.png); background-size:cover; background-repeat:no-repeat; height:100vh
-	}
-</style>
-<!-- homepage serch menu and button  -->
-<div class="searchbar col-md-12">
-<div style="height: 10vh;display:inline-block;"></div>
-<div class="form-row">
-	<div class="input-group mb-2 mx-sm-3 col-sm-4" id="prefetch1">
-	
-		<input type="text" name="jobtitle" id="jobtitle" placeholder="Job Title" class="form-control mx-sm-3 searchboxhome typeahead"
-			aria-label="Sizing example input" style="width:170px; border-radius:0px;" aria-describedby="inputGroup-sizing-default" />
-			<i id="filtersubmit" class="fa fa-search"></i>
-	</div>
-	<div class="input-group mb-2 mx-sm-3 col-sm-4" id="prefetch2">
-		<input type="text" name="joblocation" id="joblocation" placeholder="Job location"
-			class="searchboxhome mx-sm-3 form-control searchboxhome" aria-label="Sizing example input"
-			aria-describedby="inputGroup-sizing-default">
-	</div>
-	<button type="submit" onclick="searchfunc()" class="btn btn-success mb-2 clo-sm-2 form-control" style="float: left;">Search Job</button>
-</div>
 
-</div>
-<div id="result" style="height:200px;overflow: auto;" class="my-auto mr-auto"></div>
-<!-- homepage serch menu and button ends here -->
-<script type="text/javascript">
-$(document).ready(function(){
-var sample_data = new Bloodhound({
-datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-queryTokenizer: Bloodhound.tokenizers.whitespace,
-prefetch1: '<?php echo base_url() ?>index.php/autocomplete/fetch',
-remote:{
-	url: '<?php echo base_url() ?>index.php/autocomplete/fetch/%QUERY',
-	wildcard: '%QUERY'
-}	
-});
-$('#prefetch1 .typeahead').typeahead(null, {
-	name:'sample_data',
-	display: 'name',
-	source:sample_data,
-	limit:10,
-	templates:{
-		suggestion:Handlebars.compile('<div class="row"><div class="col-sm-12 style="padding-right: 5px; padding-left: 5px;">{{name}}</div></div>')
-	}
-});
+			<!-- start banner Area -->
+			<section class="banner-area relative" id="home">	
+				<div class="overlay overlay-bg"></div>
+				<div class="container">
+					<div class="row fullscreen d-flex align-items-center justify-content-center">
+						<div class="banner-content col-lg-12">
+							<h1 class="text-white">
+								<span>1500+</span> Jobs posted last week				
+							</h1>	
+							<form action="search.html" class="serach-form-area">
+								<div class="row justify-content-center form-wrap">
+									<!-- <div class="col-lg-10 form-cols prefetch1"> -->
+									<div class="form-cols col-lg-10" id="prefetch1">
+										<input type="text"class="typeahead form-control searchboxhome" id="jobtitle" name="search" placeholder="what are you looking for?">
+									</div>
+									<div class="col-lg-2 form-cols">
+									    <button type="button" class="btn btn-info" onclick="searchfunc()">
+									      <span class="lnr lnr-magnifier"></span> Search
+									    </button>
+									</div>								
+								</div>
+							</form>	
+							<style>
+							.twitter-typeahead{
+								position: inherit !important;
+								display: flex !important;
+							}
+							</style>
+							<p class="text-white"> <span>Search by tags:</span> Tecnology, Business, Consulting, IT Company, Design, Development</p>
+						</div>											
+					</div>
+				</div>
+			</section>
+			<!-- End banner Area -->	
 
-	
-});
-</script >
+			<!-- Start features Area -->
+			<section class="features-area">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-3 col-md-6">
+							<div class="single-feature">
+								<h4>Searching</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6">
+							<div class="single-feature">
+								<h4>Applying</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6">
+							<div class="single-feature">
+								<h4>Security</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing.
+								</p>
+							</div>
+						</div>
+						<div class="col-lg-3 col-md-6">
+							<div class="single-feature">
+								<h4>Notifications</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing.
+								</p>
+							</div>
+						</div>																		
+					</div>
+				</div>	
+			</section>
+			<!-- End features Area -->
+			
+			<!-- Start popular-post Area -->
+			<section class="popular-post-area pt-100">
+				<div class="container">
+					<div class="row align-items-center">
+						<div class="active-popular-post-carusel">
+							<div class="single-popular-post d-flex flex-row">
+								<div class="thumb">
+									<img class="img-fluid" src="<?php echo base_url()?>bt/img/p1.png" alt="">
+									<a class="btns text-uppercase" href="#">view job post</a>
+								</div>
+								<div class="details">
+									<a href="#"><h4>Creative Designer</h4></a>
+									<h6>Los Angeles</h6>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis.
+									</p>
+								</div>
+							</div>	
+							<div class="single-popular-post d-flex flex-row">
+								<div class="thumb">
+									<img src="<?php echo base_url()?>bt/img/p2.png" alt="">
+									<a class="btns text-uppercase" href="#">view job post</a>
+								</div>
+								<div class="details">
+									<a href="#"><h4>Creative Designer</h4></a>
+									<h6>Los Angeles</h6>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis.
+									</p>
+								</div>
+							</div>
+							<div class="single-popular-post d-flex flex-row">
+								<div class="thumb">
+									<img src="<?php echo base_url()?>bt/img/p1.png" alt="">
+									<a class="btns text-uppercase" href="#">view job post</a>
+								</div>
+								<div class="details">
+									<a href="#"><h4>Creative Designer</h4></a>
+									<h6>Los Angeles</h6>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis.
+									</p>
+								</div>
+							</div>	
+							<div class="single-popular-post d-flex flex-row">
+								<div class="thumb">
+									<img src="<?php echo base_url()?>bt/img/p2.png" alt="">
+									<a class="btns text-uppercase" href="#">view job post</a>
+								</div>
+								<div class="details">
+									<a href="#"><h4>Creative Designer</h4></a>
+									<h6>Los Angeles</h6>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis.
+									</p>
+								</div>
+							</div>	
+							<div class="single-popular-post d-flex flex-row">
+								<div class="thumb">
+									<img src="<?php echo base_url()?>bt/img/p1.png" alt="">
+									<a class="btns text-uppercase" href="#">view job post</a>
+								</div>
+								<div class="details">
+									<a href="#"><h4>Creative Designer</h4></a>
+									<h6>Los Angeles</h6>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis.
+									</p>
+								</div>
+							</div>	
+							<div class="single-popular-post d-flex flex-row">
+								<div class="thumb">
+									<img src="<?php echo base_url()?>bt/img/p2.png" alt="">
+									<a class="btns text-uppercase" href="#">view job post</a>
+								</div>
+								<div class="details">
+									<a href="#"><h4>Creative Designer</h4></a>
+									<h6>Los Angeles</h6>
+									<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temporinc ididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis.
+									</p>
+								</div>
+							</div>																																							
+						</div>
+					</div>
+				</div>	
+			</section>
+			<!-- End popular-post Area -->
+			
+			<!-- Start feature-cat Area -->
+			<section class="feature-cat-area pt-100" id="category">
+				<div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="menu-content pb-60 col-lg-10">
+							<div class="title text-center">
+								<h1 class="mb-10">Featured Job Categories</h1>
+								<p>Who are in extremely love with eco friendly system.</p>
+							</div>
+						</div>
+					</div>						
+					<div class="row">
+						<div class="col-lg-2 col-md-4 col-sm-6">
+							<div class="single-fcat">
+								<a href="category.html">
+									<img src="<?php echo base_url()?>bt/img/o1.png" alt="">
+								</a>
+								<p>Accounting</p>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-6">
+							<div class="single-fcat">
+								<a href="category.html">
+									<img src="<?php echo base_url()?>bt/img/o2.png" alt="">
+								</a>
+								<p>Development</p>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-6">
+							<div class="single-fcat">
+								<a href="category.html">
+									<img src="<?php echo base_url()?>bt/img/o3.png" alt="">
+								</a>
+								<p>Technology</p>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-6">
+							<div class="single-fcat">
+								<a href="category.html">
+									<img src="<?php echo base_url()?>bt/img/o4.png" alt="">
+								</a>
+								<p>Media & News</p>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-6">
+							<div class="single-fcat">
+								<a href="category.html">
+									<img src="<?php echo base_url()?>bt/img/o5.png" alt="">
+								</a>
+								<p>Medical</p>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-4 col-sm-6">
+							<div class="single-fcat">
+								<a href="category.html">
+									<img src="<?php echo base_url()?>bt/img/o6.png" alt="">
+								</a>
+								<p>Goverment</p>
+							</div>			
+						</div>																											
+					</div>
+				</div>	
+			</section>
+			<!-- End feature-cat Area -->
+			
+			<!-- Start post Area -->
+			<section class="section-gap">
+				
+			</section>
+			<!-- End post Area -->
+				
 
-<script>
-
- //load_data();
-
- function load_data(query)
- {
-  $.ajax({
-   url:"<?php echo base_url(); ?>index.php/Title_search/fetch",
-   method:"POST",
-   data:{query:query},
-   success:function(data){
-    localStorage.setItem('myItem',data);
-	window.location.href = "index.php/Title_search/show";
-   }
-  });
- }
-
-function searchfunc(){
-  var search = document.getElementById('jobtitle').value;
-  if(search != '')
-  {
-   load_data(search);
-  }
-  else
-  {
-   load_data();
-  }
-}
-
-</script>
+			<!-- Start callto-action Area -->
+			<section class="callto-action-area section-gap" id="join">
+				<div class="container">
+					<div class="row d-flex justify-content-center">
+						<div class="menu-content col-lg-9">
+							<div class="title text-center">
+								<h1 class="mb-10 text-white">Join us today without any hesitation</h1>
+								<p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+								<a class="primary-btn" href="#">I am a Candidate</a>
+								<a class="primary-btn" href="#">I am an Employer</a>
+							</div>
+						</div>
+					</div>	
+				</div>	
+			</section>
+			<!-- End calto-action Area -->
+<!-- new homepage serch menu and button ends here -->
